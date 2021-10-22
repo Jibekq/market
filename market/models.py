4549dd98317dc3fd403f89d9b2ad11c3a086c9aa
Jibekq <jibeknasipbek@gmail.com>
2021-10-22 16:01:04 +0300

    feedback2

--- market/models.py
+++ market/models.py
@@ -4,5 +4,8 @@ class Register(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
 
+    def __str__(self):
+        return  self.surname
+
 
   
--- market/views.py
+++ market/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 from .forms import  RegistrationForm
 
 def helloworld(request):
-    return render(request, 'start.html')
+    return render(request, 'registration.html')
 
 def register(request):
     form = RegistrationForm(request.POST or None)
     message = 'Fill the blank'
     if request.method =='POST' and form.is_valid():
-        message = 'Good'
+        message = 'Your name and surname was successfully uploaded'
         form.save()
-        return render(request, 'start.html', {'form': form, 'message':message})
-    return render(request, 'start.html', {'form': form, 'message':message})
+        return render(request, 'registration.html', {'form': form, 'message':message})
+    return render(request, 'registration.html', {'form': form, 'message':message})
     
 
